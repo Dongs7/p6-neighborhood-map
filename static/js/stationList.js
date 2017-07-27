c4290dc6c55af07df94060b33d440149e0edf601
Roy Chung <roychung7@gmail.com>
2017-07-27 05:19:41 -0700

    New Commit

---
+++ static/js/stationList.js
@@ -0,0 +1,427 @@
+var stations = [{
+    line: ['Millenium'],
+    title: 'Lafarge Lake-Douglas Station',
+    location: {
+      lat: 49.2855835,
+      lng: -122.791644
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Lincoln Station',
+    location: {
+      lat: 49.2804617,
+      lng: -122.7941565
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Coquitlam Central Station',
+    location: {
+      lat: 49.2748251,
+      lng: -122.800624
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Inlet Centre Station',
+    location: {
+      lat: 49.2772516,
+      lng: -122.8281842
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Moody Centre Station',
+    location: {
+      lat: 49.2779497,
+      lng: -122.8459078
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Burquitlam Station',
+    location: {
+      lat: 49.261381,
+      lng: -122.8898
+    }
+  },
+  {
+    line: ['Millenium', 'Expo'],
+    title: 'Lougheed Town Centre Station',
+    location: {
+      lat: 49.2485016,
+      lng: -122.896991
+    }
+  },
+  {
+    line: ['Millenium', 'Expo'],
+    title: 'Production Way-University Station',
+    location: {
+      lat: 49.2534047,
+      lng: -122.9181552
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Lake City Way Station',
+    location: {
+      lat: 49.2546304,
+      lng: -122.9392344
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Sperling / Burnaby Lake Station',
+    location: {
+      lat: 49.2592013,
+      lng: -122.9640792
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Holdem Station',
+    location: {
+      lat: 49.2647455,
+      lng: -122.9821662
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'BrentWood Station',
+    location: {
+      lat: 49.2664083,
+      lng: -123.0017616
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Gilmore Station',
+    location: {
+      lat: 49.2649809,
+      lng: -123.0135117
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Rupert Station',
+    location: {
+      lat: 49.2608105,
+      lng: -123.0330238
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'Renfrew Station',
+    location: {
+      lat: 49.2588952,
+      lng: -123.045092
+    }
+  },
+  {
+    line: ['Millenium', 'Expo'],
+    title: 'Commercial-Broadway Station',
+    location: {
+      lat: 49.2626441,
+      lng: -123.0692566
+    }
+  },
+  {
+    line: ['Millenium'],
+    title: 'VCC-Clark Station',
+    location: {
+      lat: 49.2657013,
+      lng: -123.0791219
+    }
+  },
+
+  {
+    line: ['Expo', 'Canada'],
+    title: 'Waterfront Station Expo Line',
+    location: {
+      lat: 49.2859597,
+      lng: -123.1116501
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Burrard Station',
+    location: {
+      lat: 49.2856399,
+      lng: -123.1201878
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Granville Station',
+    location: {
+      lat: 49.283276,
+      lng: -123.1161203
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Stadium–Chinatown Station',
+    location: {
+      lat: 49.2793024,
+      lng: -123.1091541
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Main Street–Science World Station',
+    location: {
+      lat: 49.2731564,
+      lng: -123.1004548
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Nanaimo Station',
+    location: {
+      lat: 49.2483697,
+      lng: -123.0558707
+    }
+  },
+  {
+    line: ['Expo'],
+    title: '29th Avenue Station',
+    location: {
+      lat: 49.2443298,
+      lng: -123.0462404
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Joyce–Collingwood Station',
+    location: {
+      lat: 49.238502,
+      lng: -123.0320564
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Patterson Station',
+    location: {
+      lat: 49.2297874,
+      lng: -123.0126753
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Metrotown Station',
+    location: {
+      lat: 49.2257737,
+      lng: -123.0037583
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Royal Oak Station',
+    location: {
+      lat: 49.2200706,
+      lng: -122.9884628
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Edmonds Station',
+    location: {
+      lat: 49.2122617,
+      lng: -122.9591408
+    }
+  },
+  {
+    line: ['Expo'],
+    title: '22nd Street Station',
+    location: {
+      lat: 49.1999948,
+      lng: -122.9491245
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'New Westminster Station',
+    location: {
+      lat: 49.2014242,
+      lng: -122.9127257
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Columbia Station',
+    location: {
+      lat: 49.2048287,
+      lng: -122.9060995
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Sapperton Station',
+    location: {
+      lat: 49.2246869,
+      lng: -122.8893704
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Braid Station',
+    location: {
+      lat: 49.2332626,
+      lng: -122.8828625
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Scott Road Station',
+    location: {
+      lat: 49.2044164,
+      lng: -122.8741659
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Gateway Station',
+    location: {
+      lat: 49.1988985,
+      lng: -122.8506622
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'Surrey Central Station',
+    location: {
+      lat: 49.1894944,
+      lng: -122.8478491
+    }
+  },
+  {
+    line: ['Expo'],
+    title: 'King George Station',
+    location: {
+      lat: 49.182755,
+      lng: -122.8446418
+    }
+  },
+
+  {
+    line: ['Canada'],
+    title: 'Vancouver City Centre Station',
+    location: {
+      lat: 49.2824832,
+      lng: -123.118555
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Yaletown-Roundhouse Station',
+    location: {
+      lat: 49.2745211,
+      lng: -123.1218596
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Olympic Village Station',
+    location: {
+      lat: 49.2665117,
+      lng: -123.1157381
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Broadway-City Hall Station',
+    location: {
+      lat: 49.2629521,
+      lng: -123.1144996
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'King Edward Station',
+    location: {
+      lat: 49.2492562,
+      lng: -123.1158517
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Oakridge-41st Avenue Station',
+    location: {
+      lat: 49.2331568,
+      lng: -123.116637
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Langara-49th Avenue Station',
+    location: {
+      lat: 49.2263277,
+      lng: -123.1160772
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Marine Drive Station',
+    location: {
+      lat: 49.2095948,
+      lng: -123.1169071
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Bridgeport Station',
+    location: {
+      lat: 49.1955291,
+      lng: -123.1260478
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Aberdeen Station',
+    location: {
+      lat: 49.1839547,
+      lng: -123.1364705
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Landsdowne Station',
+    location: {
+      lat: 49.1747384,
+      lng: -123.1365696
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Richmond-Brighouse Station',
+    location: {
+      lat: 49.1680805,
+      lng: -123.1362376
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Templeton Station',
+    location: {
+      lat: 49.1966853,
+      lng: -123.1464086
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'Sea Island Station',
+    location: {
+      lat: 49.1930546,
+      lng: -123.1579993
+    }
+  },
+  {
+    line: ['Canada'],
+    title: 'YVR-Airport Station',
+    location: {
+      lat: 49.1942659,
+      lng: -123.1781014
+    }
+  },
+];
